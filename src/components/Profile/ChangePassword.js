26fecc69c9352e45317a804ba5c77772c7a48820
Federico Villa <fedevilla88@gmail.com>
2020-10-10 15:57:48 -0300

    Refactor ChangePassword.js

--- src/components/Profile/ChangePassword.js
+++ src/components/Profile/ChangePassword.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import styled from "styled-components";
-import { compose } from "recompose";
 import * as R from "ramda";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Button, Form, Input, Row } from "antd";
 import { changePassword, CHANGE_PASSWORD } from "../../store/actions/user";
 import { isLoading } from "../../utils/actions";
@@ -14,30 +13,42 @@ const Container = styled.div`
   margin: 20px;
 `;
 
-const ChangePassword = ({ form, changePassword, loading }) => {
+const ChangePassword = ({ form }) => {
+  const loading = useSelector((state) => isLoading(CHANGE_PASSWORD, state));
+  const dispatch = useDispatch();
+
   const { getFieldDecorator, resetFields } = form;
 
   const [confirmDirty, setConfirmDirty] = useState(false);
 
-  const handleConfirmBlur = e => {
-    const { value } = e.target;
-    setConfirmDirty({ confirmDirty: confirmDirty || !!value });
-  };
+  const handleConfirmBlur = useCallback(
+    (e) => {
+      const { value } = e.target;
+      setConfirmDirty({ confirmDirty: confirmDirty || !!value });
+    },
+    [confirmDirty]
+  );
 
-  const compareToFirstPassword = (rule, value, callback) => {
-    if (value && value !== form.getFieldValue("newPassword")) {
-      callback("Las contraseñas no coinciden");
-    } else {
+  const compareToFirstPassword = useCallback(
+    (rule, value, callback) => {
+      if (value && value !== form.getFieldValue("newPassword")) {
+        callback("Las contraseñas no coinciden");
+      } else {
+        callback();
+      }
+    },
+    [form]
+  );
+
+  const validateToNextPassword = useCallback(
+    (rule, value, callback) => {
+      if (value && confirmDirty) {
+        form.validateFields(["confirm"], { force: true });
+      }
       callback();
-    }
-  };
-
-  const validateToNextPassword = (rule, value, callback) => {
-    if (value && confirmDirty) {
-      form.validateFields(["confirm"], { force: true });
-    }
-    callback();
-  };
+    },
+    [confirmDirty, form]
+  );
 
   return (
     <Form layout="horizontal">
@@ -48,13 +59,13 @@ const ChangePassword = ({ form, changePassword, loading }) => {
             rules: [
               {
                 required: true,
-                message: "Por favor ingrese su contraseña"
+                message: "Por favor ingrese su contraseña",
               },
               {
                 min: 6,
-                message: "Debe tener al menos 6 caracteres"
-              }
-            ]
+                message: "Debe tener al menos 6 caracteres",
+              },
+            ],
           })(<Input.Password />)}
         </Form.Item>
         <Form.Item label="Nueva contraseña" hasFeedback>
@@ -62,16 +73,16 @@ const ChangePassword = ({ form, changePassword, loading }) => {
             rules: [
               {
                 required: true,
-                message: "Por favor ingrese su nueva contraseña"
+                message: "Por favor ingrese su nueva contraseña",
               },
               {
                 min: 6,
-                message: "Debe tener al menos 6 caracteres"
+                message: "Debe tener al menos 6 caracteres",
               },
               {
-                validator: validateToNextPassword
-              }
-            ]
+                validator: validateToNextPassword,
+              },
+            ],
           })(<Input.Password onBlur={handleConfirmBlur} />)}
         </Form.Item>
         <Form.Item label="Confirmar contraseña" hasFeedback>
@@ -79,12 +90,12 @@ const ChangePassword = ({ form, changePassword, loading }) => {
             rules: [
               {
                 required: true,
-                message: "Por favor confirme su contraseña"
+                message: "Por favor confirme su contraseña",
               },
               {
-                validator: compareToFirstPassword
-              }
-            ]
+                validator: compareToFirstPassword,
+              },
+            ],
           })(<Input.Password onBlur={handleConfirmBlur} />)}
         </Form.Item>
         <Row type="flex" justify="start">
@@ -93,11 +104,11 @@ const ChangePassword = ({ form, changePassword, loading }) => {
               type="primary"
               icon="save"
               loading={loading}
-              onClick={ev => {
+              onClick={(ev) => {
                 ev.preventDefault();
                 form.validateFields(async (err, values) => {
                   if (!err) {
-                    await changePassword(R.dissoc("confirm", values));
+                    await dispatch(changePassword(R.dissoc("confirm", values)));
                     resetFields();
                   }
                 });
@@ -112,14 +123,4 @@ const ChangePassword = ({ form, changePassword, loading }) => {
   );
 };
 
-const enhancer = compose(
-  Form.create(),
-  connect(
-    state => ({
-      loading: isLoading(CHANGE_PASSWORD, state)
-    }),
-    { changePassword }
-  )
-);
-
-export default enhancer(ChangePassword);
+export default Form.create()(ChangePassword);
